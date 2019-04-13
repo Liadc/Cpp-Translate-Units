a8ec04a1e1eb42a84a036763ad73950f0fd47de9
Liad Cohen - Ubuntu VM <liad.code@gmail.com>
2019-04-14 00:10:37 +0300

    changed length case

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -29,19 +29,19 @@ double PhysicalNumber::translateOther(const PhysicalNumber& other){
         switch((int)other.typeID % 3){
             case 0: //Length case
                 if(((int)this->typeID == 0 && (int)other.typeID == 3) || ((int)this->typeID == 3 && (int)other.typeID == 0)){  
-                    return other.units/100*1.0f;
+                    mult = 100.0f;
                     break;
                 }else if(((int)this->typeID == 0 && (int)other.typeID == 6) || ((int)this->typeID == 6 && (int)other.typeID == 0)){  
-                    return other.units/100000*1.0f;
+                    mult = 1000.0f;
                     break;
                 }
-                return (double)other.units*(pow(mult,(-dif)));
+                return (double)(other.units/mult)*(pow(1,(double)(-dif)));
                 break;
             case 1: //Mass cases
-                return (double)other.units*(pow(1000.0f,(-dif)));
+                return (double)other.units*(pow(1000.0f,(double)(-dif)));
                 break;
             case 2: //Time case
-                return (double)other.units*(pow(60.0f,(-dif)));
+                return (double)other.units*(pow(60.0f,(double)(-dif)));
                 break;
             }
     }
